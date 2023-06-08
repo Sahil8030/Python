cd8a0f66472d700bd6fe1548dd6c6929d3e540aa
Sahil Mahajan <135845276+Sahil8030@users.noreply.github.com>
2023-06-09 01:31:37 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,6 @@
+character_name="John"
+print("There once was a man named " + character_name+" , ")
+print("he was 70 years old")
+character_name = "TOM"
+print("he really liked name " + character_name+ ",")
+print("but didn't like being 70")
